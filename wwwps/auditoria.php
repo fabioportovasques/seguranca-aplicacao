fa3818cb8bbb6026695a8ea99c3c7f9f8312d097
fabioportovasques <fabioportovasques321@gmail.com>
2021-05-19 17:54:04 -0300

    criacao de variavel para armezenar a data e hora e corrigir o problema de horarios de logs errados

--- wwwps/auditoria.php
+++ wwwps/auditoria.php
@@ -5,10 +5,12 @@
 	/*
 		Insere os dados na tabela auditoria
 	*/
+
+	$date = date('Y-m-d H:i');	
 	$insert  = "insert into auditoria values (
 	'".$_SESSION['usuario']."', 
 	'".$_SERVER['PHP_SELF']."',
-	 datetime('now'), 
+	 '".$date."', 
 	 '".$_SERVER['REMOTE_ADDR']."'
 	 );";
 	$conexao->exec($insert);
